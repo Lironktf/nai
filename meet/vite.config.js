b797868b7c45a0f9aeb667458c5570a275bd0482
Lironktf <Lironktf@gmail.com>
2026-03-12 23:14:43 -0400

    vite config change

--- meet/vite.config.js
+++ meet/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/meet-panel',
-  server: {
-    port: 5174,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/",
+    server: {
+      port: 5174,
+    },
+  };
 });
